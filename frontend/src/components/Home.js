9f3d621e7948645427d9e9b06716441bfaa5f9cd
Ridwan Oladapo <olaitanoladapo29@gmail.com>
2024-07-11 19:13:15 +0100

    add search to books and authors frontend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,20 +3,41 @@ import api from '../services/api';
 
 const Home = () => {
     const [books, setBooks] = useState([]);
+    const [query, setQuery] = useState('');
 
     useEffect(() => {
-        api.get('/books')
-            .then(response => {
-                setBooks(response.data);
-            })
-            .catch(error => {
-                console.error('There was an error fetching the books!', error);
-            });
-    }, []);
+        if(query) {
+            api.get(`/books/search?query=${query}`)
+                .then(response => {
+                    setBooks(response.data);
+                })
+                .catch(error => {
+                    console.error('There was an error fetching the books!', error);
+                });
+        } else {
+            api.get('/books')
+                .then(response => {
+                    setBooks(response.data);
+                })
+                .catch(error => {
+                    console.error('There was an error fetching the books!', error);
+                });
+        }
+    }, [query]);
+
+    const handleSearch = (e) => {
+        setQuery(e.target.value);
+    };
 
     return (
         <div>
             <h1>Books</h1>
+            <input
+                type="text"
+                placeholder="Search for books or authors..."
+                value={query}
+                onChange={handleSearch}
+            />
             <ul>
                 {books.map(book => (
                     <li key={book.id}>{book.title} by {book.author.name}</li>
